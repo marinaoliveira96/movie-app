ead569701c76afd9f123dee5e1a56f0442dbe3b1
Marina <marina09oliveira@gmail.com>
2021-01-17 19:24:19 -0300

    Router

--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Potato() {
-    return <div>Hiii</div>;
-}
-
-export default Potato;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div className="movie__title">
-            <div className="movie">
-                <img src={poster} alt={title} title={title} />
-            </div>
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <p className="movie__summary">{summary}</p>
                 <ul className="movie__genres">
                     {genres.map((genre, index) => (
-                        <li key={index} className="genres_genre">
+                        <li key={index} className="genres__genre">
                             {genre}
                         </li>
                     ))}
                 </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
