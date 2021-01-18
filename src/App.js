e15cb7baa6c315791e85ae7c545e00117235cf22
Marina <marina09oliveira@gmail.com>
2021-01-18 15:34:06 -0300

    Redirecting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Detail from './routes/Detail';
 import Navigation from './components/Navigation';
-
 import './App.css';
-import Detail from './components/Detail';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
-            <Route path="/movie-detail" component={Detail} />
-        </BrowserRouter>
+            <Route path="/movie/:id" component={Detail} />
+        </HashRouter>
     );
 }
 
