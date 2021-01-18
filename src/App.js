b79323581deb70d630fcc5c0266558f056ca34a4
Marina <marina09oliveira@gmail.com>
2021-01-18 15:08:25 -0300

    Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './routes/About';
 import Navigation from './components/Navigation';
 
 import './App.css';
+import Detail from './components/Detail';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/movie-detail" component={Detail} />
         </BrowserRouter>
     );
 }
